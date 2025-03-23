a5c98d11214416232d56302b6a2333e7d80f4373
ardasennn <ardaaasennn@gmail.com>
2025-03-23 15:58:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const response = document.getElementById("response");
+
+// "Evet" butonuna basılırsa emoji göster
+yesBtn.addEventListener("click", function() {
+    response.innerText = "😊";
+    sendData("Evet");
+});
+
+// "Hayır" butonuna basılırsa buton kaçar
+noBtn.addEventListener("mouseover", function() {
+    const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
+
+noBtn.addEventListener("click", function() {
+    response.innerText = "😂";
+    sendData("Hayır");
+});
+
+// Google Sheets'e veri gönderme fonksiyonu
+function sendData(choice) {
+    fetch("https://script.google.com/macros/s/AKfycbybha84gxI3JLLsl2spqsqaAzcgvBw3n169P_YtJ8W8PLjAH3uJgaobfZgPoezXJaHoCQ/exec?choice=" + choice)
+    .then(response => console.log("Kaydedildi: " + choice))
+    .catch(error => console.error("Hata: ", error));
+}
